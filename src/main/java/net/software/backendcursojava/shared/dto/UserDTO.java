86926fd9366be8368a32a931f565141b390fcbef
Jose danilo caro b <danilocarob@gmail.com>
2021-09-16 02:24:43 -0500

    agregamos el userDTO

---
+++ src/main/java/net/software/backendcursojava/shared/dto/UserDTO.java
@@ -0,0 +1,77 @@
+package net.software.backendcursojava.shared.dto;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+    private static final Long serialversionUID = 1L;
+
+    private Long id;
+    private String userId;
+    private String firtsname;
+    private String lastName;
+    private String email;
+    private String password;
+    private String encryptedPassword;
+
+    public static Long getSerialversionuid() {
+        return serialversionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirtsname() {
+        return firtsname;
+    }
+
+    public void setFirtsname(String firtsname) {
+        this.firtsname = firtsname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+}
