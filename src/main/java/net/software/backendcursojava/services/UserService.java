413406c859b243cf5b02d8ea59631089b1028e40
Jose danilo caro b <danilocarob@gmail.com>
2021-10-04 21:19:41 -0500

    Se valida quen o se repita idusuario en el registro y se pone libreria de jwt

--- src/main/java/net/software/backendcursojava/services/UserService.java
+++ src/main/java/net/software/backendcursojava/services/UserService.java
@@ -1,5 +1,7 @@
 package net.software.backendcursojava.services;
 
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,12 +18,15 @@ public class UserService implements UserServiceInterface {
 
     @Override
     public UserDTO createUser(UserDTO userDto) {
+        if (userRepository.findUserByEmail(userDto.getEmail()) != null)
+            throw new RuntimeException("");
 
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(userDto, userEntity);
 
         userEntity.setEncryptedPassword("testpassword");
-        userEntity.setUserId("testUserId");
+        UUID userId = UUID.randomUUID();
+        userEntity.setUserId(userId.toString());
 
         UserEntity storedUserDetail = userRepository.save(userEntity);
         UserDTO userToReturn = new UserDTO();
